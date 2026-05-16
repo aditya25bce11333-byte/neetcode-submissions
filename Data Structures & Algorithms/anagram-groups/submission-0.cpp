5f38e358c81d4f1e983960d1a730ccb9c3c95a1d
aditya25bce11333-byte <aditya.25bce11333@vitbhopal.ac.in>
2026-05-16 16:50:55 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map < string , vector<string>> res;
+
+        for(string s : strs){
+            string temp = s;
+            sort ( temp.begin() , temp.end());
+            res[temp].push_back(s);
+        }
+        vector <vector<string>> result;
+        for ( auto it: res){
+            result.push_back(it.second);
+        }
+        return result; 
+    }
+};
